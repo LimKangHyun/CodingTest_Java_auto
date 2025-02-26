3de5cba3dc41a3454c68e246bd5fc9a076266014
kanghyun <113881138+LimKangHyun@users.noreply.github.com>
2025-02-26 17:03:39 +0900

    [Gold III] Title: 웜홀, Time: 348 ms, Memory: 37928 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -1,8 +1,17 @@
 import java.io.*;
 import java.util.*;
 
+class Node {
+    int target;
+    int time;
+    
+    Node(int target, int time) {
+        this.target = target;
+        this.time = time;
+    }
+}
 public class Main {
-    static List<int []> list;
+    static List<List<Node>> list;
     static int N;
     static int dist[];
     private static final int INF = 500 * 10_000;
@@ -18,21 +27,21 @@ public class Main {
             int M = Integer.parseInt(st.nextToken());
             int W = Integer.parseInt(st.nextToken());
             list = new ArrayList<>();
+            for (int i = 0; i <= N; i++) {
+                list.add(new ArrayList<>());
+            } 
             for (int i = 0; i < M + W; i++) {
                 st = new StringTokenizer(br.readLine());
                 int s = Integer.parseInt(st.nextToken());
                 int e = Integer.parseInt(st.nextToken());
                 int t = Integer.parseInt(st.nextToken());
                 if (i < M) {
-                    list.add(new int[] {s,e,t});
-                    list.add(new int[] {e,s,t});
+                    list.get(s).add(new Node(e, t));
+                    list.get(e).add(new Node(s, t));
                 } else {
-                    list.add(new int[] {s,e,-t});
+                    list.get(s).add(new Node(e, -t));
                 }
             }
-            for (int i = 1; i <= N; i++) {
-                list.add(new int[] {0, i, 0});
-            } 
             if (bellmanFord()) {
                 bw.write(String.valueOf("YES\n"));
             } else {
@@ -45,23 +54,29 @@ public class Main {
         dist = new int[N+1];
         Arrays.fill(dist, INF);
         dist[0] = 0;
+        boolean check = false;
         for (int i = 0; i < N; i++) {//모든 최단경로 계산
-            for (int[] node : list) {
-                int start = node[0];
-                int end = node[1];
-                int time = node[2];
-                if (dist[start] != INF && dist[end] > dist[start] + time) {
-                    dist[end] = dist[start] + time;
+            check = false;
+            for(int j = 1; j <= N; j++) {
+                for (Node node : list.get(j)) {
+                    if (dist[node.target] > dist[j] + node.time) {
+                        dist[node.target] = dist[j] + node.time;
+                        check = true;
+                    }
+                } 
+            }
+            if(!check) {
+                break;
+            }
+        } 
+        if(check) {
+            for (int j = 1; j <= N; j++) {
+                for (Node node : list.get(j)) { //한번 더 계산했을 때, 최단경로가 변경된다면 음수사이클 존재
+                    if (dist[node.target] > dist[j] + node.time) {
+                        return true;
+                    }
                 }
             } 
-        } 
-        for (int[] node : list) { //한번 더 계산했을 때, 최단경로가 변경된다면 음수사이클 존재
-            int start = node[0];
-            int end = node[1];
-            int time = node[2];
-            if (dist[start] != INF && dist[end] > dist[start] + time) {
-                return true;
-            }
         }
         return false;
     }
