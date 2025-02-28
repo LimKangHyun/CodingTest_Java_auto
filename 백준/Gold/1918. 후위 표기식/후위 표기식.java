1989ee7976bea7246e1ea04ad292893bb1be6abb
kanghyun <113881138+LimKangHyun@users.noreply.github.com>
2025-02-28 23:18:42 +0900

    [Gold II] Title: 후위 표기식, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Gold/1918. 후위 표기식/후위 표기식.java
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.java
@@ -9,31 +9,33 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         String input = br.readLine();
+        Stack<Character> oper = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        
         char[] c = new char[input.length()];
         for (int i = 0; i < c.length; i++) {
             c[i] = input.charAt(i);
-        } 
-        Stack<Character> oper = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < c.length; i++) {
-            if (c[i] >= 'A' && c[i] <= 'Z') {
-                sb.append(c[i] + "");
-            } else {
-                if (c[i] == '(') {
-                    oper.push(c[i]);
-                } else if (c[i] == ')') {
-                    while(!oper.isEmpty() && oper.peek() != '(') {
-                        sb.append(oper.pop());
-                    }
-                    if (!oper.isEmpty()) {
-                        oper.pop();
-                    } 
-                } else {
+            switch(c[i]) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
                     while(!oper.isEmpty() && precedence(oper.peek()) >= precedence(c[i])) {
                         sb.append(oper.pop());
                     }
                     oper.push(c[i]);
-                }
+                    break;
+                case '(':
+                    oper.push('(');
+                    break;
+                case ')':
+                    while(!oper.isEmpty() && oper.peek() != '(') {
+                        sb.append(oper.pop());
+                    }
+                    oper.pop();
+                    break;
+                default:
+                    sb.append(c[i]);
             }
         } 
         while(!oper.isEmpty()) {
@@ -47,8 +49,10 @@ public class Main {
             return 2;
         } else if (op == '+' || op == '-') {
             return 1;
-        } else {
+        } else if (op == '(' || op == ')') {
             return 0;
+        } else {
+            return -1;
         }
     }
 }
