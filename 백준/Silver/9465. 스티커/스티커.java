56475ea73659c28f75be4d93df162d838fd46945
kanghyun <113881138+LimKangHyun@users.noreply.github.com>
2025-03-03 17:11:50 +0900

    [Silver I] Title: 스티커, Time: 608 ms, Memory: 112728 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
+		StringBuilder sb = new StringBuilder();
 		sticker = Integer.parseInt(br.readLine());
 		for (int s = 0; s < sticker; s++) {
 		    int num = Integer.parseInt(br.readLine());
@@ -22,9 +22,9 @@ public class Main {
 		    } 
 		    dp[1][1] = arr[1][1];
 		    dp[2][1] = arr[2][1];
-		    bw.write(String.valueOf(findMax(num)));
-		    bw.write("\n");
+		    sb.append(findMax(num)).append("\n");
 		} 
+		bw.write(sb.toString());
 		bw.flush();
 	}
 	private static int findMax(int num) {
