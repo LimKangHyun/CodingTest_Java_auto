aa52db86299d017f176fb88dcf027f6c2ad8e6ed
kanghyun <113881138+LimKangHyun@users.noreply.github.com>
2025-03-05 19:41:57 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 504 ms, Memory: 49956 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,8 +3,7 @@ import java.util.*;
 
 public class Main {
     static int N, M;
-    static boolean[] visit;
-    static List<int[]>[] list;
+    static int parent[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -12,42 +11,49 @@ public class Main {
 		String[] input = br.readLine().split(" ");
 		N = Integer.parseInt(input[0]);
 		M = Integer.parseInt(input[1]);
-		visit = new boolean[N+1];
-		list = new ArrayList[N+1];
+		parent = new int[N+1];
 		for (int i = 1; i <= N; i++) {
-		    list[i] = new ArrayList<>();
+		    parent[i] = i;
 		} 
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
 		for (int i = 0; i < M; i++) {
 		    StringTokenizer st = new StringTokenizer(br.readLine());
 		    int a = Integer.parseInt(st.nextToken());
 		    int b = Integer.parseInt(st.nextToken());
 		    int c = Integer.parseInt(st.nextToken());
-		    list[a].add(new int[] {b, c});
-		    list[b].add(new int[] {a, c});
+		    pq.add(new int[] {a, b, c});
 		}
-		bw.write(String.valueOf(primSpanning()));
+		int total = 0;
+		for (int i = 0; i < M; i++) {
+		    int[] current = pq.poll();
+		    int start = find(current[0]);
+		    int end = find(current[1]);
+		    if (!isSameParent(start, end)) {
+		        total += current[2];
+		        union(current[0], current[1]);
+		    } 
+		} 
+		bw.write(String.valueOf(total));
 		bw.flush();
 	}
-	private static int primSpanning() {
-	    PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Long.compare(a[1], b[1]));
-	    pq.offer(new int[] {1, 0});
-	    int total = 0;
-	    while(!pq.isEmpty()) {
-	        int[] current = pq.poll();
-	        int currentNode = (int) current[0];
-	        int currentWeight = current[1];
-	        if (!visit[currentNode]) {
-	            visit[currentNode] = true;
-	            total += currentWeight;
-	            for (int[] next : list[(int) current[0]]) {
-    	            int nextNode = (int) next[0];
-    	            int nextWeight = next[1];
-    	            if(!visit[nextNode]) {
-    	                pq.offer(new int[] {nextNode, nextWeight});
-    	            }
-    	        }
-	        } 
+	private static void union(int x, int y) {
+	    x = find(x);
+	    y = find(y);
+	    if (x != y) {
+	        parent[y] = x;  
+	    } 
+	}
+	private static int find(int x) {
+	    if(parent[x] == x) return x;
+	    return parent[x] = find(parent[x]); 
+	}
+	private static boolean isSameParent(int x, int y) {
+	    x = find(x);
+	    y = find(y);
+	    if (x == y) {
+	        return true;
+	    } else {
+	        return false;
 	    }
-	    return total;
 	}
 }
