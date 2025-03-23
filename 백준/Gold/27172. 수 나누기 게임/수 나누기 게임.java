3e7c2faafb9e0fb8ff87de5cc222d8dbc8c7eb8a
kanghyun <113881138+LimKangHyun@users.noreply.github.com>
2025-03-23 17:43:04 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 368 ms, Memory: 31380 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -1,46 +1,42 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
+    private static int N;
+    private static int[] cards;
+    private static int[] idx;
+    private static int[] score;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         
-        int N = Integer.parseInt(br.readLine());
-
-        boolean[] visit = new boolean[1000001];
-        int[] num = new int[N+1];
-        int[] save = new int[1000001];
-
-        StringTokenizer st=new StringTokenizer(br.readLine());
-        for(int i = 1; i <= N; i++){
-            int t=Integer.valueOf(st.nextToken());
-            visit[t] = true;
-            num[i] = t;
-        }
-
-        for(int i = 1; i <= N; i++){
-            int t = num[i];
-            for(int j = 1; j <= Math.sqrt(t); j++){
-                if(t%j != 0){
-                    continue;
-                }
-                if(visit[j]){
-                    save[j]++;
-                    save[t]--;
-                }
-                if(j*j != t && visit[t/j]){
-                    save[t/j]++;
-                    save[t]--;
-                }
-            }
-
-        }
-        for(int i = 1; i <= N; i++){
-            sb.append(save[num[i]] + " ");
+        N = Integer.parseInt(br.readLine());
+        cards = new int[N+1];
+        idx = new int[1000001];
+        score = new int[N+1];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+            idx[cards[i]] = i;
+            
+        } 
+        findScore();
+        for (int i = 1; i <= N; i++) {
+            sb.append(score[i]).append(" ");
         }
         bw.write(sb.toString());
         bw.flush();
+    }
+    private static void findScore() {
+        for (int i = 1; i <= N; i++) {
+            for (int j = cards[i] * 2; j < 1000001; j += cards[i]) {
+                if (idx[j] >= 1) {
+                    score[i]++;
+                    score[idx[j]]--;
+                } 
+            } 
+        }
     }
 }
