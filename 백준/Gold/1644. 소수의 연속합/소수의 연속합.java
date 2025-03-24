3e2d16652de7759355cedeacc9b6b4b5ec4195cb
kanghyun <113881138+LimKangHyun@users.noreply.github.com>
2025-03-24 19:58:45 +0900

    [Gold III] Title: 소수의 연속합, Time: 272 ms, Memory: 32880 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		boolean[] isPrime = new boolean[N+1];
+		Arrays.fill(isPrime, 2, N+1, true);
+		List<Integer> list = new ArrayList<>();
+		
+		for (int i = 2; i * i <= N; i++) {
+		    if(isPrime[i]) {
+		        for (int j = i * i; j <= N; j += i) {
+    		        isPrime[j] = false;
+    		    }     
+		    }
+		}
+		for (int i = 2; i <= N; i++) {
+		    if(isPrime[i]) {
+		        list.add(i);
+		    }
+		} 
+		// 마지막 조건이 isPrime[N]이 true이면 +1해서 출력
+		int[] arr = list.stream().mapToInt(i -> i).toArray();
+		int answer = CountSubSum(N, arr);
+		bw.write(String.valueOf(answer));
+		bw.flush();
+	}
+	private static int CountSubSum (int N, int[] arr) {
+	    int left = 0;
+	    int sum = 0;
+	    int count = 0;
+	    
+	    for (int right = 0; right < arr.length; right++) {
+	        sum += arr[right];
+	        while(sum > N && left <= right) {
+	            sum -= arr[left];
+	            left++;
+	        }
+	        if (sum == N) {
+	            count++;
+	        } 
+	    } 
+	    return count;
+	}
+}
