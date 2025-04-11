6281a58393738bab218b3e17407e48bf1d0f0508
kanghyun <113881138+LimKangHyun@users.noreply.github.com>
2025-04-11 17:48:12 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -21,11 +21,13 @@ public class Main {
 		for (int i = 0; i < K; i++) {
 			int current = seq[i];
 			if (set.contains(current)) continue;
+
 			if (set.size() < N) {
 				set.add(current);
 			} else {
 				int lastUseIndex = -1;
 				int removal = -1;
+
 				for (int plugged : set) {
 					int nextUse = Integer.MAX_VALUE;
 
@@ -35,6 +37,8 @@ public class Main {
 							break;
 						}
 					}
+					// 만약 set의 모든 원소가 i+1부터 재등장하지 않는 경우 
+					// nextUse는 Integer.MAX_VALUE가 되어 가장 첫번째값이 언플러그 됨
 					if (nextUse > lastUseIndex) {
 						lastUseIndex = nextUse;
 						removal = plugged;
