e00599133bcbe219faf1e7e13388de41b94a8136
kanghyun <113881138+LimKangHyun@users.noreply.github.com>
2025-05-03 01:02:10 +0900

    [level 3] Title: 여행경로, Time: 11.92 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -6,7 +6,7 @@ class Solution {
     private static boolean isFinish;
     public String[] solution(String[][] tickets) {
         answer = new String[tickets.length + 1];
-        visit = new boolean[tickets.length + 1];
+        visit = new boolean[tickets.length];
         //tickets 정렬(맨 처음 재귀에 들어갈 원소를 찾아야 하므로)
         Arrays.sort(tickets, (a, b) -> {
             if (a[0].equals("ICN")) return -1; // 반환값이 음수이면 a, b 유지
@@ -14,26 +14,19 @@ class Solution {
             return a[1].compareTo(b[1]); // 둘다 아니면 2번째 원소로 비교
         });
         System.out.println(Arrays.deepToString(tickets));
-        for(String[] ticket : tickets) {
-            answer[0] = ticket[0];
-            Arrays.fill(visit, false); // visit 배열 초기화
-            visit[0] = true;
-            dfs(tickets, ticket, 1);
-            if (isFinish) break;
-        }        
+        dfs("ICN", "ICN", tickets, 0);
         return answer;
     }
-    private static void dfs(String[][] tickets, String[] curTicket, int depth) {
+    private static void dfs(String departure, String path, String[][] tickets, int depth) {
         if (depth == tickets.length) {
-            answer[depth] = curTicket[1];
+            answer = path.split(",");
             isFinish = true;
             return;
         }
-        for(int i = 1; i < tickets.length; i++) {
-            if (curTicket[1].equals(tickets[i][0]) && !visit[i]) {
+        for(int i = 0; i < tickets.length; i++) {
+            if (!visit[i] && tickets[i][0].equals(departure)) {
                 visit[i] = true;
-                answer[depth] = tickets[i][0];
-                dfs(tickets, tickets[i], depth+1);
+                dfs(tickets[i][1], path + "," + tickets[i][1], tickets, depth+1);
                 if (isFinish) break;
                 visit[i] = false;
             }
