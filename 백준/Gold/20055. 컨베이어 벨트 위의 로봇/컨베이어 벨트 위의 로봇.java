21cc282ae4c6b5353428e1e4ad95f3d7317b7e8c
kanghyun <113881138+LimKangHyun@users.noreply.github.com>
2025-05-03 13:58:21 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 232 ms, Memory: 17188 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -1,9 +1,19 @@
 import java.io.*;
 import java.util.*;
-
+// 리팩토링
 public class Main {
+    static class Belt {
+        int dur;
+        boolean isRobot;
+        
+        public Belt(int dur) {
+            this.dur = dur;
+            this.isRobot = false;
+        }
+    }
     private static int N, K;
-    private static List<int[]> belt; // [내구도, 로봇유무]
+    private static List<Belt> belt;
+    private static int zeroCount = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -14,30 +24,41 @@ public class Main {
         belt = new ArrayList<>();
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N * 2; i++) {
-            belt.add(new int[] {Integer.parseInt(st.nextToken()), 0});
+            belt.add(new Belt (Integer.parseInt(st.nextToken())));
         } 
         int stage = 0;
-        int zeroCount = 0;
         while(zeroCount < K) {
             stage++;
-            belt.add(0, belt.remove(belt.size() - 1));
-            if (belt.get(N-1)[1] == 1) belt.get(N-1)[1] = 0; 
-            for (int i = N - 2; i >= 0; i--) {
-                if (belt.get(i)[1] == 1 && belt.get(i+1)[1] == 0 && belt.get(i+1)[0] >= 1) {
-                    belt.get(i)[1]--;
-                    belt.get(i+1)[1]++;
-                    belt.get(i+1)[0]--;
-                    if (belt.get(i+1)[0] == 0) zeroCount++; 
-                }
-            } 
-            if (belt.get(N-1)[1] == 1) belt.get(N-1)[1] = 0; 
-            if (belt.get(0)[1] == 0 && belt.get(0)[0] >= 1) {
-                belt.get(0)[1]++;
-                belt.get(0)[0]--;
-                if (belt.get(0)[0] == 0) zeroCount++; 
-            } 
+            moveBelt();
+            removeEndRobot();
+            moveRobots();
+            removeEndRobot();
+            placeRobot();
         }
         bw.write(String.valueOf(stage));
         bw.flush();
+    }
+    private static void moveBelt() {
+        belt.add(0, belt.remove(belt.size() - 1));
+    }
+    private static void moveRobots() {
+        for (int i = N - 2; i >= 0; i--) {
+            if (belt.get(i).isRobot && !belt.get(i+1).isRobot && belt.get(i+1).dur >= 1) {
+                belt.get(i).isRobot = false;
+                belt.get(i+1).isRobot = true;
+                belt.get(i+1).dur--;
+                if (belt.get(i+1).dur == 0) zeroCount++; 
+            }
+        } 
+    }
+    private static void removeEndRobot() {
+        if (belt.get(N-1).isRobot) belt.get(N-1).isRobot = false; 
+    }
+    private static void placeRobot() {
+        if (!belt.get(0).isRobot && belt.get(0).dur >= 1) {
+            belt.get(0).isRobot = true;
+            belt.get(0).dur--;
+            if (belt.get(0).dur == 0) zeroCount++; 
+        } 
     }
 }
