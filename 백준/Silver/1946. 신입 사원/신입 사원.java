09805f5a73485a9b9323d4f95e6f41a8fc8e8e9f
kanghyun <113881138+LimKangHyun@users.noreply.github.com>
2025-05-03 20:26:54 +0900

    [Silver I] Title: 신입 사원, Time: 1808 ms, Memory: 307308 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -3,37 +3,42 @@ import java.util.*;
 
 public class Main {
     private static int N;
-    private static int[][] arr;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		while(T-- > 0) {
-		    N = Integer.parseInt(br.readLine());
-		    arr = new int[N][2];
-		    
-		    for (int i = 0; i < N; i++) {
-		        String[] input = br.readLine().split(" ");
-		        arr[i][0] = Integer.parseInt(input[0]);
-		        arr[i][1] = Integer.parseInt(input[1]);
-		    } 
-		    Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
-		    sb.append(findMaxCount() + "\n");
-		}
-		bw.write(sb.toString());
-		bw.flush();
-	}
-	private static int findMaxCount() {
-	    int min = arr[0][1];
-	    int count = 1;
-	    for (int i = 1; i < N; i++) {
-	        if (arr[i][1] < min) {
-	            min = arr[i][1];
-	            count++;
-	        } 
-	    } 
-	    return count;
-	}
+    private static List<int[]> arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            N = Integer.parseInt(br.readLine());
+            arr = new ArrayList<>();
+
+            for (int i = 0; i < N; i++) {
+                String[] input = br.readLine().split(" ");
+                int doc = Integer.parseInt(input[0]);
+                int interview = Integer.parseInt(input[1]);
+                arr.add(new int[]{doc, interview});
+            }
+
+            arr.sort(Comparator.comparingInt(a -> a[0]));
+            sb.append(findMaxCount()).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int findMaxCount() {
+        int min = arr.get(0)[1];
+        int count = 1;
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr.get(i)[1] < min) { // min보다 크다면, arr[0]보다 서류, 면접 둘다 점수가 크므로 X
+                min = arr.get(i)[1];
+                count++;
+            }
+        }
+        return count;
+    }
 }
