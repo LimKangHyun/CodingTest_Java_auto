6ffd5a73f6e020fc8d124c7f3c2bff40e23a0c58
kanghyun <limkh06910691@gmail.com>
2025-06-05 14:37:17 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14284 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,6 +1,7 @@
 import java.io.*;
 
 public class Main {
+    private static int MOD = 1234567891;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -9,8 +10,11 @@ public class Main {
 		String input = br.readLine();
 		
 		long sum = 0;
+		long power = 1;
 		for (int i = 0; i < input.length(); i++) {
-		    sum += (input.charAt(i) - 'a' + 1) * Math.pow(31, i);
+		    int charVal = input.charAt(i) - 'a' + 1;
+		    sum = (sum + charVal * power) % MOD;
+		    power = (power * 31) % MOD;
 		} 
 		bw.write(String.valueOf(sum));
 		bw.flush();
