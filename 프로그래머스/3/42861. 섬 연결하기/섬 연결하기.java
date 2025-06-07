1ca3965b98f570d7d1c5d012ad21a83792e6f3d8
kanghyun <limkh06910691@gmail.com>
2025-06-07 15:44:24 +0900

    [level 3] Title: 섬 연결하기, Time: 0.57 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,11 +1,13 @@
 import java.util.*;
 
 class Solution {
-    // MST 프림
+    // MST 크루스칼
     static class Edge implements Comparable<Edge> {
+        int from;
         int to;
         int weight;
-        Edge (int to, int weight) {
+        Edge (int from, int to, int weight) {
+            this.from = from;
             this.to = to;
             this.weight = weight;
         }
@@ -15,38 +17,39 @@ class Solution {
         }
     }
     public int solution(int n, int[][] costs) {
-        List<List<Edge>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
+        int total = 0;
+        int count = 0; // 이 후 간선 생략용
+        List<Edge> graph = new ArrayList<>();
         for (int[] cost : costs) {
             int from = cost[0];
             int to = cost[1];
             int weight = cost[2];
-            graph.get(from).add(new Edge(to, weight));
-            graph.get(to).add(new Edge(from, weight));
+            graph.add(new Edge(to, from, weight));
         }
-        return prim(n, graph);
-    }
-    private static int prim(int n, List<List<Edge>> graph) {
-        int total = 0;
-        boolean[] visit = new boolean[n];
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.offer(new Edge(0, 0));
-        while(!pq.isEmpty()) {
-            Edge current = pq.poll();
-            if(!visit[current.to]) {
-                visit[current.to] = true;
-                total += current.weight;
-                for(Edge e : graph.get(current.to)) {
-                    int nextTo = e.to;
-                    int nextWeight = e.weight;
-                    if (!visit[nextTo]) {
-                        pq.offer(new Edge(nextTo, nextWeight));
-                    }
-                }
+        Collections.sort(graph);
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+        for (Edge e : graph) {
+            // 이미 이어져있다면 연결하지 않기
+            if (union(parent, e.from, e.to)) {
+                total += e.weight;
+                count++;
+                if (count == n - 1) break;
             }
         }
         return total;
+    }
+    private static int find(int[] parent, int x) {
+        if(parent[x] != x) {
+            x = find(parent, parent[x]);
+        }
+        return parent[x];
+    }
+    private static boolean union(int[] parent, int x, int y) {
+        x = find(parent, x);
+        y = find(parent, y);
+        if (x == y) return false;
+        parent[y] = x;
+        return true;
     }
 }
