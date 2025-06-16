6d0c5e2879d57adc0e5e5ae79e731026b7fc5566
kanghyun <limkh06910691@gmail.com>
2025-06-16 17:58:16 +0900

    [level 1] Title: 폰켓몬, Time: 4.56 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,8 +3,8 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-        for(int i = 0; i < nums.length; i++) {
-            set.add(nums[i]);
+        for(int num : nums) {
+            set.add(num);
         }
         return Math.min(set.size(), nums.length/2);
     }
