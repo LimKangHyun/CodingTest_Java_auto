c418e5f65e79eb3e5bdf3b5544d91064324b9b27
kanghyun <limkh06910691@gmail.com>
2025-08-25 15:35:51 +0900

    [Bronze III] Title: ACM 호텔, Time: 132 ms, Memory: 16004 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -2,27 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-
-		int t = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-		while(t-- > 0) {
-		    st = new StringTokenizer(br.readLine());
-		    int h = Integer.parseInt(st.nextToken());
-		    int w = Integer.parseInt(st.nextToken());
-		    int n = Integer.parseInt(st.nextToken());
-		    int floor = n % h;
-		    int room = n / h + 1;
-		    if (floor == 0) {
-		        floor = h;
-		        room--;
-		    } 
-		    sb.append(String.format("%d%02d\n", floor, room));
-		}
+		StringBuilder sb = new StringBuilder();
 		
+		int T = Integer.parseInt(br.readLine());
+		while(T-- > 0) {
+		    st = new StringTokenizer(br.readLine());
+		    int H = Integer.parseInt(st.nextToken());
+		    int W = Integer.parseInt(st.nextToken());
+		    int N = Integer.parseInt(st.nextToken());
+		    
+		    int floor = N % H * 100;
+		    int roomNumber = N / H + 1;
+		    if (floor == 0) {
+		        floor = H * 100;
+		        roomNumber--;
+		    }
+		    sb.append(floor + roomNumber + "\n");
+		}
 		bw.write(sb.toString());
 		bw.flush();
 	}
