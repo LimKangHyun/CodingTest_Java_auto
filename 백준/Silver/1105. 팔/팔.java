a82539afab435263d6d4779adff8943d4e1b8495
kanghyun <limkh06910691@gmail.com>
2025-08-29 12:15:04 +0900

    [Silver I] Title: 팔, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/1105. 팔/팔.java
+++ 백준/Silver/1105. 팔/팔.java
@@ -19,18 +19,12 @@ public class Main {
 	    int numL = Integer.parseInt(L);
 	    int numR = Integer.parseInt(R);
 	    int count = 0;
-	    // 8, 80, 800, 8000 대 인 경우 최소 1개씩은 있으니까 자리수를 확인해서 8이 들어가는지 확인해서
 	    for (int i = L.length() - 1; i >= 0; i--) {
 	        int div = (int) Math.pow(10, i);
 	        if (numL / div == numR / div) {
-	            if (numL / div == 8) {
-	                numL %= div;
-                    numR %= div;
-                    count++;
-	            } else {
-	                numL %= div;
-                    numR %= div;
-	            }
+	            if (numL / div == 8) count++;
+	            numL %= div;
+                numR %= div;
 	        } else break;
 	    } 
 	    return count;
