2db7f69d8792484a01b00ba2187ea2cc42134e65
kanghyun <limkh06910691@gmail.com>
2025-09-02 08:38:56 +0900

    [Bronze V] Title: 크냐?, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		while(true) {
+		    st = new StringTokenizer(br.readLine());
+		    int a = Integer.parseInt(st.nextToken());
+		    int b = Integer.parseInt(st.nextToken());
+		    if (a == 0) break;
+		    sb.append(a > b ? "Yes\n" : "No\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+	}
+}
