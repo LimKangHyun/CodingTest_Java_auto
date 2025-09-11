fc4c1cd793ecc254a380f89ecf32a029b7b92b64
kanghyun <limkh06910691@gmail.com>
2025-09-11 18:20:59 +0900

    [Gold V] Title: 별자리 찾기, Time: 160 ms, Memory: 16624 KB -BaekjoonHub

--- 백준/Gold/5588. 별자리 찾기/별자리 찾기.java
+++ 백준/Gold/5588. 별자리 찾기/별자리 찾기.java
@@ -2,49 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static class Star {
+        int x;
+        int y;
+        
+        Star(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Star other = (Star) obj;
+            return this.x == other.x && this.y == other.y;
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         
         int m = Integer.parseInt(br.readLine());
-        int[][] arr = new int[m][2];
+        Star[] arr = new Star[m];
         int[][] diff = new int[m][2];
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            arr[i][0] = x;
-            arr[i][1] = y;
+            arr[i] = new Star(x, y);
             if (i > 0) {
-                diff[i][0] = arr[i][0] - arr[0][0];
-                diff[i][1] = arr[i][1] - arr[0][1];
+                diff[i][0] = arr[i].x - arr[0].x;
+                diff[i][1] = arr[i].y - arr[0].y;
             } 
         } 
         int n = Integer.parseInt(br.readLine());
-        Set<String> set = new HashSet<>();
+        Set<Star> set = new HashSet<>();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            set.add(x + "," + y);
+            set.add(new Star(x, y));
         } 
-        for (String coordinate : set) {
-            String[] star = coordinate.split(",");
-            int x = Integer.parseInt(star[0]);
-            int y = Integer.parseInt(star[1]);
-            
+        for (Star coordinate : set) {
             boolean found = true;
             for (int i = 1; i < m; i++) {
-                int targetX = x + diff[i][0];
-                int targetY = y + diff[i][1];
-                if (!set.contains(targetX + "," + targetY)) {
+                Star target = new Star(coordinate.x + diff[i][0], coordinate.y + diff[i][1]);
+                if (!set.contains(target)) {
                     found = false;
                     break;
                 }
             } 
             if (found) { // found가 true인 경우, 해당 star가 arr[0][0]과 arr[0][1]과 대응되므로
-                bw.write((x - arr[0][0]) + " " + (y - arr[0][1]));
+                bw.write((coordinate.x - arr[0].x) + " " + (coordinate.y - arr[0].y));
                 break;
             } 
         } 
