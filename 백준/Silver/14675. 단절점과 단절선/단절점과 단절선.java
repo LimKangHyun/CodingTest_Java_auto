0c450d10b275ab96a40747cab214effc973e6fb9
kanghyun <limkh06910691@gmail.com>
2025-09-15 19:43:50 +0900

    [Silver I] Title: 단절점과 단절선, Time: 416 ms, Memory: 54236 KB -BaekjoonHub

--- 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.java
+++ 백준/Silver/14675. 단절점과 단절선/단절점과 단절선.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static List<List<Integer>> graph = new ArrayList<>();
+    private static int[] degree;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -10,26 +10,20 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(br.readLine());
-		for (int i = 0; i <= N; i++) {
-		    graph.add(new ArrayList<>());
-		} 
+		degree = new int[N + 1];
 		for (int i = 0; i < N - 1; i++) {
 		    st = new StringTokenizer(br.readLine());
 		    int a = Integer.parseInt(st.nextToken());
 		    int b = Integer.parseInt(st.nextToken());
-		    graph.get(a).add(b);
-		    graph.get(b).add(a);
+		    degree[a]++;
+		    degree[b]++;
 		} 
 		int q = Integer.parseInt(br.readLine());
 		while (q-- > 0) {
 		    st = new StringTokenizer(br.readLine());
 		    int t = Integer.parseInt(st.nextToken());
 		    int k = Integer.parseInt(st.nextToken());
-		    if (t == 1) {
-		        sb.append(graph.get(k).size() == 1 ? "no" : "yes").append("\n");
-		        continue;
-		    }
-		    sb.append("yes").append("\n");
+		    sb.append(t == 1 && degree[k] == 1 ? "no" : "yes").append("\n");
 		}
 		bw.write(sb.toString());
 		bw.flush();
