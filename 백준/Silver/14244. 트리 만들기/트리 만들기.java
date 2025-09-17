2b5592ed9f56dd9e4958bc6df6cc800b77a41997
kanghyun <limkh06910691@gmail.com>
2025-09-17 19:24:08 +0900

    [Silver IV] Title: 트리 만들기, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/14244. 트리 만들기/트리 만들기.java
+++ 백준/Silver/14244. 트리 만들기/트리 만들기.java
@@ -13,14 +13,14 @@ public class Main {
 		
 		if (m == 2) {
 		    for (int i = 0; i < n - 1; i++) {
-		        sb.append(i + " " + (i + 1)).append("\n");
+		        sb.append(i).append(" ").append(i + 1).append("\n");
 		    } 
 		} else {
 		    for (int i = 1; i <= m; i++) { // m개 가지 뻗기
-    		    sb.append(0 + " " + i).append("\n");
+    		    sb.append(0).append(" ").append(i).append("\n");
     		} 
     		for (int i = m + 1; i < n; i++) { // 가지에 붙이기
-    		    sb.append((i - 1) + " " + i).append("\n");
+    		    sb.append(i - 1).append(" ").append(i).append("\n");
     		} 
 		}
 		bw.write(sb.toString());
