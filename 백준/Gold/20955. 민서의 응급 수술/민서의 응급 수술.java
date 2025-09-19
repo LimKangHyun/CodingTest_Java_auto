e1121d74ea71004db576b1951442b73ad62313b4
kanghyun <limkh06910691@gmail.com>
2025-09-20 03:15:59 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 364 ms, Memory: 49216 KB -BaekjoonHub

--- 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.java
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.java
@@ -2,9 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static List<List<Integer>> graph;
-    private static boolean[] visit;
-    private static int nodeCount, edgeCount;
+    private static int[] parent;
+    private static int cycleCount = 0;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -14,39 +13,32 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		graph = new ArrayList<>();
-		for (int i = 0; i <= N; i++) {
-		    graph.add(new ArrayList<>());
-		} 
-		visit = new boolean[N + 1];
+	    parent = new int[N + 1];
+	    for (int i = 1; i <= N; i++) {
+	        parent[i] = i;
+	    } 
 		while(M-- > 0) {
 		    st = new StringTokenizer(br.readLine());
 		    int u = Integer.parseInt(st.nextToken());
 		    int v = Integer.parseInt(st.nextToken());
-		    graph.get(u).add(v);
-		    graph.get(v).add(u);
+		    if (!union(u, v)) cycleCount++;
 		}
-		int setCount = 0;
-		int removeCount = 0; // 사이클 없애기 용
+		HashSet<Integer> set = new HashSet<>();
 		for (int i = 1; i <= N; i++) {
-		    if (visit[i]) continue;
-		    nodeCount = 0;
-		    edgeCount = 0;
-		    dfs(i);
-		    edgeCount /= 2;
-		    removeCount += edgeCount - (nodeCount - 1); // 트리는 노드가 N, 간선이 N - 1
-		    setCount++;
+		    set.add(find(i));
 		} 
-		bw.write(String.valueOf(removeCount + setCount - 1));
+		bw.write(String.valueOf(cycleCount + set.size() - 1));
 		bw.flush();
 	}
-	private static void dfs(int node) {
-	    visit[node] = true;
-	    nodeCount++;
-	    for (int next : graph.get(node)) {
-	        edgeCount++;
-	        if (visit[next]) continue;
-	        dfs(next);
-	    } 
+	private static int find(int x) {
+	    if (parent[x] == x) return x;
+	    return parent[x] = find(parent[x]);
+	}
+	private static boolean union(int x, int y) {
+	    int rootA = find(x);
+	    int rootB = find(y);
+	    if (rootA == rootB) return false;
+	    parent[rootB] = rootA;
+	    return true;
 	}
 }
