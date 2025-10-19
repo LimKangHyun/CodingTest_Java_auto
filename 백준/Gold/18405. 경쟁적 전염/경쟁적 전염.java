efeb0ae26a3283e5c7261a588cc4cf4cd8af1f0f
kanghyun <limkh06910691@gmail.com>
2025-10-19 15:14:31 +0900

    [Gold V] Title: 경쟁적 전염, Time: 204 ms, Memory: 20456 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -5,7 +5,7 @@ public class Main {
     private static int N, K, S, X, Y;
     private static String[] input;
     private static int[][] arr;
-    private static PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[3] != b[3] ? a[3] - b[3] : a[2] - b[2]);
+    private static List<int[]> viruses = new ArrayList<>();
     private static int[] dx = {-1, 1, 0, 0};
     private static int[] dy = {0, 0, -1, 1};
 	public static void main(String[] args) throws IOException {
@@ -20,9 +20,10 @@ public class Main {
 		    input = br.readLine().split(" ");
 		    for (int j = 0; j < N; j++) {
 		        arr[i][j] = Integer.parseInt(input[j]);
-		        if (arr[i][j] != 0) pq.offer(new int[] {i, j, arr[i][j], 0});
+		        if (arr[i][j] != 0) viruses.add(new int[] {i, j, arr[i][j]});
 		    }
 		}
+		viruses.sort((a, b) -> a[2] - b[2]);
 		input = br.readLine().split(" ");
 		S = Integer.parseInt(input[0]);
 		X = Integer.parseInt(input[1]) - 1;
@@ -32,17 +33,20 @@ public class Main {
 		bw.flush();
 	}
 	private static void fillArr() {
-	    while(!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            if (cur[3] == S) continue;
-            for (int d = 0; d < 4; d++) {
-                int nx = cur[0] + dx[d];
-                int ny = cur[1] + dy[d];
-                if (nx < 0 || ny < 0|| nx >= N || ny >= N) continue;
-                if (arr[nx][ny] != 0) continue;
-                arr[nx][ny] = cur[2];
-                pq.offer(new int[] {nx, ny, cur[2], cur[3] + 1});
-            }
+	    Queue<int[]> queue = new ArrayDeque<>(viruses);
+	    while(!queue.isEmpty() && S-- > 0) {
+	        int size = queue.size();
+	        for (int i = 0; i < size; i++) {
+	            int[] cur = queue.poll();
+                for (int d = 0; d < 4; d++) {
+                    int nx = cur[0] + dx[d];
+                    int ny = cur[1] + dy[d];
+                    if (nx < 0 || ny < 0|| nx >= N || ny >= N) continue;
+                    if (arr[nx][ny] != 0) continue;
+                    arr[nx][ny] = cur[2];
+                    queue.offer(new int[] {nx, ny, cur[2]});
+                }
+	        }
 	    }
 	}
 }
