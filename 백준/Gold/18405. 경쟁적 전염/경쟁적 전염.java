cca89c7ff637141e8e6a7029622b936151cfc670
kanghyun <limkh06910691@gmail.com>
2025-10-19 15:07:49 +0900

    [Gold V] Title: 경쟁적 전염, Time: 252 ms, Memory: 21876 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -2,26 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Virus implements Comparable<Virus>{
-        
-        int x, y, num, time;
-        
-        Virus(int x, int y, int num, int time) {
-            this.x = x;
-            this.y = y;
-            this.num = num;
-            this.time = time;
-        }
-        @Override
-        public int compareTo(Virus o) {
-            if (this.time != o.time) return this.time - o.time;
-            return this.num - o.num;
-        }
-    }
     private static int N, K, S, X, Y;
     private static String[] input;
     private static int[][] arr;
-    private static PriorityQueue<Virus> pq = new PriorityQueue<>();
+    private static PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[3] != b[3] ? a[3] - b[3] : a[2] - b[2]);
     private static int[] dx = {-1, 1, 0, 0};
     private static int[] dy = {0, 0, -1, 1};
 	public static void main(String[] args) throws IOException {
@@ -36,7 +20,7 @@ public class Main {
 		    input = br.readLine().split(" ");
 		    for (int j = 0; j < N; j++) {
 		        arr[i][j] = Integer.parseInt(input[j]);
-		        if (arr[i][j] != 0) pq.offer(new Virus(i, j, arr[i][j], 0));
+		        if (arr[i][j] != 0) pq.offer(new int[] {i, j, arr[i][j], 0});
 		    }
 		}
 		input = br.readLine().split(" ");
@@ -49,15 +33,15 @@ public class Main {
 	}
 	private static void fillArr() {
 	    while(!pq.isEmpty()) {
-            Virus cur = pq.poll();
-            if (cur.time == S) continue;
+            int[] cur = pq.poll();
+            if (cur[3] == S) continue;
             for (int d = 0; d < 4; d++) {
-                int nx = cur.x + dx[d];
-                int ny = cur.y + dy[d];
+                int nx = cur[0] + dx[d];
+                int ny = cur[1] + dy[d];
                 if (nx < 0 || ny < 0|| nx >= N || ny >= N) continue;
                 if (arr[nx][ny] != 0) continue;
-                arr[nx][ny] = cur.num;
-                pq.offer(new Virus(nx, ny, cur.num, cur.time + 1));
+                arr[nx][ny] = cur[2];
+                pq.offer(new int[] {nx, ny, cur[2], cur[3] + 1});
             }
 	    }
 	}
