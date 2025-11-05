06f4cfb85bb78020392cd6899dd4e7075b0e0856
kanghyun <limkh06910691@gmail.com>
2025-11-05 22:06:36 +0900

    [Gold V] Title: 용액, Time: 324 ms, Memory: 30360 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -2,36 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-
-        int N = Integer.parseInt(br.readLine());
-        long[] arr = new long[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
-        }
-        int left = 0;
-        int right = N - 1;
-        int lValue = 0;
-        int rValue = 0;
-        long min = Long.MAX_VALUE;
-        while (left < right) {
-            long sum = arr[left] + arr[right];
-            if (min > Math.abs(sum)) {
-                min = Math.abs(sum);
-                lValue = left;
-                rValue = right;
-            }
-            if (sum >= 0) {
-                right--;
-            } else {
-                left++;
-            }
-        }
-        bw.write(arr[lValue] + " " + arr[rValue]);
-        bw.flush();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		String[] input = br.readLine().split(" ");
+		for (int i = 0; i < N; i++) {
+		    arr[i] = Integer.parseInt(input[i]);
+		}
+		Arrays.sort(arr);
+		int num1 = 0;
+		int num2 = 0;
+		int left = 0;
+		int right = N - 1;
+		int result = Integer.MAX_VALUE;
+		while(left < right) {
+		    int sum = arr[left] + arr[right];
+		    if (Math.abs(sum) < result) {
+		        num1 = arr[left];
+		        num2 = arr[right];
+		        result = Math.abs(sum);
+		    }
+		    if (sum <= 0) left++;
+		    else right--;
+		}
+		bw.write(num1 + " " + num2);
+		bw.flush();
+	}
 }
