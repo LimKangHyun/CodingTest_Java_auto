a49427d5b99b5bd5e62ddfac2a1c2d9e57243a77
kanghyun <limkh06910691@gmail.com>
2025-11-10 23:10:01 +0900

    [Gold III] Title: 망가진 계산기, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/1443. 망가진 계산기/망가진 계산기.java
+++ 백준/Gold/1443. 망가진 계산기/망가진 계산기.java
@@ -4,31 +4,26 @@ import java.util.*;
 public class Main {
     private static int D, P;
     private static long max = -1;
-    private static Set<String> set = new HashSet<>();
+    private static long limit = 0;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		D = Integer.parseInt(st.nextToken());
 		P = Integer.parseInt(st.nextToken());
-		dfs(1L, 0, 2);
+		limit = (long) Math.pow(10, D);
+		dfs(1L, 0, 9);
 		bw.write(String.valueOf(max));
 		bw.flush();
 	}
 	private static void dfs(long num, int depth, int start) {
-	    if (String.valueOf(num).length() > D) return;
 	    if (depth == P) {
 	        max = Math.max(max, num);
 	        return;
 	    }
-	    
-	    String key = num + " " + depth;
-	    if (set.contains(key)) return;
-	    set.add(key);
-	    
-	    for (int i = start; i <= 9; i++) {
-	        long next = num * i;
-	        dfs(next, depth + 1, i);
+	    for (int i = start; i >= 2; i--) {
+	        if (num * i >= limit) continue;
+	        dfs(num * i, depth + 1, i);
 	    }
 	}
 }
