371927d78c30e995de1eb267cbaab08edefb298c
kanghyun <limkh06910691@gmail.com>
2025-11-24 00:17:33 +0900

    [Gold III] Title: 다리 만들기, Time: 140 ms, Memory: 15856 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -5,8 +5,8 @@ public class Main {
     static int N;
     static int minDist = Integer.MAX_VALUE;
     static int[][] map;
-    static boolean[][] visited;
-    static List<int[]> borders = new ArrayList<>();
+    static int[][] set;
+    static Queue<int[]> border = new ArrayDeque<>();
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 	public static void main(String[] args) throws IOException {
@@ -16,7 +16,7 @@ public class Main {
 		
 		N = Integer.parseInt(br.readLine());
 		map = new int[N][N];
-		visited = new boolean[N][N];
+		set = new int[N][N];
 		for (int i = 0; i < N; i++) {
 		    st = new StringTokenizer(br.readLine());
 		    for (int j = 0; j < N; j++) {
@@ -26,20 +26,17 @@ public class Main {
 		int idx = 1;
 		for (int i = 0; i < N; i++) {
 		    for (int j = 0; j < N; j++) {
-		        if (map[i][j] == 1 && !visited[i][j]) {
+		        if (map[i][j] == 1 && set[i][j] == 0) {
 		            checkIsland(i, j, idx++);
 		        }
 		    }
 		}
-		for (int[] border : borders) {
-		    checkMinDist(border);
-		}
+		checkMinDist();
 		bw.write(String.valueOf(minDist));
 		bw.flush();
 	}
 	private static void checkIsland(int x, int y, int idx) {
-	    visited[x][y] = true;
-	    map[x][y] = idx;
+	    set[x][y] = idx;
 	    Queue<int[]> queue = new ArrayDeque<>();
 	    queue.offer(new int[] {x, y});
 	    while(!queue.isEmpty()) {
@@ -50,31 +47,33 @@ public class Main {
 	            int ny = cur[1] + dy[i];
 	            if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
 	            if (map[nx][ny] == 0) isBorder = true;
-	            if (!visited[nx][ny] && map[nx][ny] == 1) {
-                    visited[nx][ny] = true;
-                    map[nx][ny] = idx;
+	            if (set[nx][ny] == 0 && map[nx][ny] == 1) {
+                    set[nx][ny] = idx;
                     queue.offer(new int[] {nx, ny});
                 }
 	        }
-	        if (isBorder) borders.add(new int[] {cur[0], cur[1], idx, 0});
+	        if (isBorder) border.add(new int[] {cur[0], cur[1]});
 	    }
 	}
-	private static void checkMinDist(int[] border) {
-	    boolean[][] visitedSea = new boolean[N][N];
-	    Queue<int[]> queue = new ArrayDeque<>();
-	    queue.offer(border);
-	    while(!queue.isEmpty()) {
-	        int[] cur = queue.poll();
+	private static void checkMinDist() {
+	    int[][] dist = new int[N][N];
+	    while(!border.isEmpty()) {
+	        int[] cur = border.poll();
+	        int x = cur[0];
+	        int y = cur[1];
 	        for (int i = 0; i < 4; i++) {
-	            int nx = cur[0] + dx[i];
-	            int ny = cur[1] + dy[i];
+	            int nx = x + dx[i];
+	            int ny = y + dy[i];
 	            if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-	            if (map[nx][ny] == 0 && !visitedSea[nx][ny]) {
-	                visitedSea[nx][ny] = true;
-	                queue.offer(new int[] {nx, ny, cur[2], cur[3] + 1});
-	            } else if (map[nx][ny] != 0 && map[nx][ny] != cur[2]) {
-	                minDist = Math.min(minDist, cur[3]);
-	                return;
+	            if (map[nx][ny] == 0 && set[nx][ny] == 0) {
+	                set[nx][ny] = set[x][y];
+	                dist[nx][ny] = dist[x][y] + 1;
+	                border.offer(new int[] {nx, ny});
+	            } else if (set[nx][ny] != set[x][y]) { // 다른 섬 번호
+	                // dist[x][y] = BFS가 진행중인 섬에서 온거리
+	                // dist[nx][ny] = 다른 섬에서 이미 확장한 거리
+	                minDist = Math.min(minDist, dist[x][y] + dist[nx][ny]);
+	                if (minDist == 1) return;
 	            }
 	        }
 	    }
