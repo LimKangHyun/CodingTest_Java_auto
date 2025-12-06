be4cb53323033a667384e75170f13ebd3f9cc467
kanghyun <limkh06910691@gmail.com>
2025-12-06 13:01:26 +0900

    [Silver II] Title: 결혼식, Time: 152 ms, Memory: 18468 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -2,54 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            adj.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            adj.get(a).add(b);
-            adj.get(b).add(a);
-        }
-
-        int cnt = bfs(N, adj);
-        System.out.println(cnt);
-    }
-
-    private static int bfs(int N, List<List<Integer>> adj) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(1);
-
-        boolean[] visited = new boolean[1 + N];
-        visited[1] = true;
-        int dist = 0;
-        int cnt = 0;
-
-        while (!q.isEmpty()) {
-            int len = q.size();
-            for (int i = 0; i < len; i++) {
-                int node = q.poll();
-                for (int next : adj.get(node)) {
-                    if (visited[next]) continue;
-                    q.offer(next);
-                    visited[next] = true;
-                    cnt++;
-                }
-            }
-            dist++;
-            if (dist == 2) break;
-        }
-        return cnt;
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+		List<Integer>[] list = new ArrayList[n + 1];
+		for (int i = 0; i <= n; i++) {
+		    list[i] = new ArrayList<>();
+		}
+		for (int i = 0; i < m; i++) {
+		    st = new StringTokenizer(br.readLine());
+		    int a = Integer.parseInt(st.nextToken());
+		    int b = Integer.parseInt(st.nextToken());
+		    list[a].add(b);
+		    list[b].add(a);
+		}
+		int dist = 0;
+		int count = 0;
+		boolean[] visited = new boolean[n + 1];
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] {1, 0});
+		visited[1] = true;
+		while(!queue.isEmpty()) {
+		    int[] cur = queue.poll();
+		    int node = cur[0];
+		    int depth = cur[1];
+		    if (depth == 2) continue;
+		    for (int next : list[node]) {
+		        if (visited[next]) continue;
+		        visited[next] = true;
+		        count++;
+		        queue.offer(new int[] {next, depth + 1});
+		    }
+		}
+		bw.write(String.valueOf(count));
+		bw.flush();
+	}
 }
