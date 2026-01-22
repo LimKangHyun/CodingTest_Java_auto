b37c7f0126c6286819a453cce2276ce3f41aa128
kanghyun <limkh06910691@gmail.com>
2026-01-22 10:52:32 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 16.65 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,17 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        int goalNum = 0;
-        int len = queue1.length * 2;
+        int n = queue1.length;
+        long total = 0;
+        int len = n * 2;
         int[] arr = new int[len];
         for (int i = 0; i < len; i++) {
-            if (i < len / 2) arr[i] = queue1[i];
-            else arr[i] = queue2[i - len / 2];
-            goalNum += arr[i];
+            if (i < n) arr[i] = queue1[i];
+            else arr[i] = queue2[i - n];
+            total += arr[i];
         }
-        goalNum /= 2;
-        System.out.println(Arrays.toString(arr));
-        int answer = -2;
-        return answer;
+        if (total % 2 != 0) return -1;
+        long goalNum = total / 2;
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += queue1[i];
+        }
+        int left = 0;
+        int right = n;
+        int count = 0;
+        while (left < right && right < len) {
+            if (sum == goalNum) return count;
+            else if (sum < goalNum) {
+                sum += arr[right++];
+            } else {
+                sum -= arr[left++];
+            }
+            count++;
+        }
+        return -1;
     }
 }
