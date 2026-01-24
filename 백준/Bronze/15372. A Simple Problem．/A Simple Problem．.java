d903f57ba051430edd16e89653c052b3234b29a9
kanghyun <limkh06910691@gmail.com>
2026-01-24 15:18:42 +0900

    [Bronze IV] Title: A Simple Problem., Time: 1628 ms, Memory: 125484 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.BigInteger;
+public class Main {
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(bf.readLine());
+		for(int i = 0; i < t; i++) {
+			BigInteger n = new BigInteger(bf.readLine());
+			System.out.println(n.multiply(n));
+		}
+	}
+}
