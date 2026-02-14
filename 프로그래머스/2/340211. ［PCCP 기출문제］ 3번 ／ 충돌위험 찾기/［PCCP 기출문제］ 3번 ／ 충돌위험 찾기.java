3a37d20fe586da80aeed06b333caf14f8541ff86
kanghyun <limkh06910691@gmail.com>
2026-02-14 15:54:24 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 482.47 ms, Memory: 415 MB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class Solution {
+    static Queue<int[]>[] record;
+    static int size;
+    static int answer = 0;
+    public int solution(int[][] points, int[][] routes) {
+        size = routes.length;
+        record = new ArrayDeque[size];
+        for (int i = 0; i < size; i++) {
+            record[i] = new ArrayDeque<>();
+        }
+        recordRoute(points, routes);
+        findCollision();
+        return answer;
+    }
+    private static void recordRoute(int[][] points, int[][] routes) {
+        for(int i = 0; i < size; i++) {
+            int from = routes[i][0] - 1;
+            int fromR = points[from][0] - 1;
+            int fromC = points[from][1] - 1;
+            record[i].add(new int[] {fromR, fromC});
+            for (int j = 1; j < routes[i].length; j++) {
+                int to = routes[i][j] - 1;
+                int toR = points[to][0] - 1;
+                int toC = points[to][1] - 1;
+                while(fromR != toR) {
+                    if (fromR < toR) {
+                        fromR++;
+                    } else {
+                        fromR--;
+                    }
+                    record[i].add(new int[] {fromR, fromC});
+                }
+                while(fromC != toC) {
+                    if (fromC < toC) {
+                        fromC++;
+                    } else {
+                        fromC--;
+                    }
+                    record[i].add(new int[] {fromR, fromC});
+                }
+            }
+        }
+    }
+    private static void findCollision() {
+        int escapeBots = 0;
+        while(escapeBots != size) {
+            escapeBots = 0;
+            int[][] map = new int[101][101];
+            for (int i = 0; i < size; i++) {
+                if (record[i].isEmpty()) {
+                    escapeBots++;
+                    continue;
+                }
+                int[] temp = record[i].poll();
+                map[temp[0]][temp[1]]++;
+            }
+            for (int i = 0; i < 101; i++) {
+                for (int j = 0; j < 101; j++) {
+                    if (map[i][j] > 1) {
+                        answer++;
+                    }
+                }
+            }
+        }
+    }
+}
