96f8b5ac63f7009e0fb5263de6cbc088839af8f8
kanghyun <limkh06910691@gmail.com>
2026-02-23 09:38:43 +0900

    [level 2] Title: 순위 검색, Time: 1056.08 ms, Memory: 193 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -8,47 +8,41 @@ class Solution {
             String[] arr = info.split(" ");
             String[] conditions = Arrays.copyOfRange(arr, 0, 4);
             int score = Integer.parseInt(arr[arr.length - 1]);
-            dfs(conditions, score, 0, new StringBuilder());
+            dfs(conditions, score, 0, "");
         }
         for (List<Integer> list : map.values()) {
             Collections.sort(list);
         }
         for (int i = 0; i < query.length; i++) {
-            StringBuilder sb = new StringBuilder();
             String q = query[i].replace(" and", "");
             String[] arr = q.split(" ");
+            String key = "";
             for (int j = 0; j < 4; j++) {
-                sb.append(arr[j]);
+                key += arr[j];
             }
             int score = Integer.parseInt(arr[arr.length - 1]);
-            if (!map.containsKey(sb.toString())) {
+            if (!map.containsKey(key)) {
                 answer[i] = 0;
                 continue;
             }
-            List<Integer> list = map.get(sb.toString());
+            List<Integer> list = map.get(key);
             int idx = lowerBound(list, score);
             answer[i] = list.size() - idx;
         }
         return answer;
     }
-    private void dfs(String[] conditions, int score, int depth, StringBuilder sb) {
+    private void dfs(String[] conditions, int score, int depth, String key) {
         if (depth == 4) {
-            List<Integer> list = map.get(sb.toString());
+            List<Integer> list = map.get(key);
             if (list == null) {
                 list = new ArrayList<>();
-                map.put(sb.toString(), list);
+                map.put(key, list);
             }
             list.add(score);
             return;
         }
-        int len = sb.length();
-        sb.append(conditions[depth]);
-        dfs(conditions, score, depth + 1, sb);
-        sb.setLength(len);
-        
-        sb.append("-");
-        dfs(conditions, score, depth + 1, sb);
-        sb.setLength(len);
+        dfs(conditions, score, depth + 1, key + conditions[depth]);
+        dfs(conditions, score, depth + 1, key + "-");
     }
     private int lowerBound(List<Integer> list, int score) {
         int left = 0;
