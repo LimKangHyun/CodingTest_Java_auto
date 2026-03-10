6bf5eae9655d0b9a5c0f57255c61228519c4e5e7
kanghyun <limkh06910691@gmail.com>
2026-03-10 10:54:36 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 2.29 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    int answer = 0;
+    public int solution(String message, int[][] spoiler_ranges) {
+        Set<String> not_important = new HashSet<>();
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < message.length(); i++) {
+            if (message.charAt(i) == ' ') {
+                end = i - 1;
+                if (!isHidden(start, end, spoiler_ranges)) {
+                    String word = message.substring(start, end + 1);
+                    not_important.add(word);
+                }
+                start = i + 1;
+            }
+        }
+        if (!isHidden(start, message.length() - 1, spoiler_ranges)) {
+            String word = message.substring(start);
+            not_important.add(word);
+        }
+        String[] words = message.split(" ");
+        for (String word : words) {
+            if (!not_important.contains(word)) {
+                not_important.add(word);
+                answer++;
+            }
+        }
+        return answer;
+    }
+    private boolean isHidden(int wordStart, int wordEnd, int[][] ranges) {
+        for (int[] range : ranges) {
+            int spoStart = range[0];
+            int spoEnd = range[1];
+            if (wordEnd >= spoStart && wordStart <= spoEnd) return true;
+        }
+        return false;
+    }
+}
