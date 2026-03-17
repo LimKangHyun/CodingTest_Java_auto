478c298e8a99958c812ed2545080defcf8f301db
kanghyun <limkh06910691@gmail.com>
2026-03-17 10:55:27 +0900

    [level 2] Title: 리프 노드 수 최대화, Time: 123.39 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/2/468372. 리프 노드 수 최대화/리프 노드 수 최대화.java
+++ 프로그래머스/2/468372. 리프 노드 수 최대화/리프 노드 수 최대화.java
@@ -1,25 +1,24 @@
 class Solution {
-    int answer = 1;
     int dist_limit;
     int split_limit;
+    int answer = 1;
     public int solution(int _dist_limit, int _split_limit) {
         dist_limit = _dist_limit;
         split_limit = _split_limit;
         dfs(1, 1, 1, 0);
         return answer;
     }
-    public void dfs(long split, long distUsed, long cur, long leaf) {
+    private void dfs(long split, long distUsed, long cur, long fixedLeaf) {
         if (distUsed > dist_limit) return;
-        answer = (int) Math.max(answer, leaf + cur);
+        answer = (int) Math.max(answer, fixedLeaf + cur);
         for (int b = 2; b <= 3; b++) {
             long nextSplit = split * b;
             if (nextSplit > split_limit) continue;
             long nextNodes = cur * b;
             long remain = dist_limit - distUsed;
             long nextCur = Math.min(remain, nextNodes);
-            long nextLeaf = leaf + nextNodes - nextCur;
+            long nextLeaf = fixedLeaf + nextNodes - nextCur;
             dfs(nextSplit, distUsed + nextCur, nextCur, nextLeaf);
         }
     }
-    
 }
