d6f1e84d7773783a9e6617f39fc112fab1979d44
kanghyun <limkh06910691@gmail.com>
2026-03-24 23:54:28 +0900

    [level 3] Title: 징검다리 건너기, Time: 33.31 ms, Memory: 64.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] stones, int k) {
+        int answer = 0;
+        int left = 1;
+        int right = Arrays.stream(stones).max().getAsInt();
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (canCross(stones, k, mid)) {
+                answer = mid;
+                left = mid + 1;
+            } else right = mid - 1;
+        }
+        return answer;
+    }
+    private boolean canCross(int[] stones, int k, int mid) {
+        int zeroCount = 0;
+        for(int stone : stones) {
+            if (stone < mid) zeroCount++;
+            else zeroCount = 0;
+            if (zeroCount >= k) return false;
+        }
+        return true;
+    }
+}
