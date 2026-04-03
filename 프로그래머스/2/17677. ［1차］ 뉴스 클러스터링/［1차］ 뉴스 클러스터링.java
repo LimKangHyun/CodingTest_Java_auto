5f49b11433ce409c8aed15d207287a816f9d4257
kanghyun <limkh06910691@gmail.com>
2026-04-03 11:10:05 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 2.21 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,35 +1,24 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String str1, String str2) {
-        int answer = 0;
-        double union = 0;
-        double intersec = 0;
+    public int solution(String str1, String str2) {        
         str1 = str1.toLowerCase();
         str2 = str2.toLowerCase();
+        double union = 0;
         Map<String, Integer> str1Map = new HashMap<>();
         for (int i = 0; i < str1.length() - 1; i++) {
             char c1 = str1.charAt(i);
             char c2 = str1.charAt(i + 1);
-            if (c1 < 'a' || c1 > 'z') {
-                continue;
-            } else if (c2 < 'a' || c2 > 'z') {
-                i++;
-                continue;
-            }
+            if (checkIsNotLetter(c1, c2)) continue;
             String str1Sub = str1.substring(i, i + 2);
             str1Map.put(str1Sub, str1Map.getOrDefault(str1Sub, 0) + 1);
             union++;
         }
+        double intersec = 0;
         for (int i = 0; i < str2.length() - 1; i++) {
             char c1 = str2.charAt(i);
             char c2 = str2.charAt(i + 1);
-            if (c1 < 'a' || c1 > 'z') {
-                continue;
-            } else if (c2 < 'a' || c2 > 'z') {
-                i++;
-                continue;
-            }
+            if (checkIsNotLetter(c1, c2)) continue;
             String str2Sub = str2.substring(i, i + 2);
             if (str1Map.containsKey(str2Sub)) {
                 int val = str1Map.get(str2Sub) - 1;
@@ -40,5 +29,8 @@ class Solution {
         }
         if (union == 0 && intersec == 0) return 65536;
         return (int) (intersec / union * 65536);
+    }
+    private boolean checkIsNotLetter(char c1, char c2) {
+        return !Character.isLetter(c1) || !Character.isLetter(c2);
     }
 }
