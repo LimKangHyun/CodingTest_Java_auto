dae3717273d4aacda9722307df9120d9b9332390
kanghyun <limkh06910691@gmail.com>
2026-06-04 11:40:49 +0900

    [level 2] Title: 디펜스 게임, Time: 115.64 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -2,21 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int k, int[] enemy) {
-        int answer = 0;
-        int round = enemy.length;
-        if (k >= round) return round;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i = 0; i < round; i++) {
-            n -= enemy[i];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < enemy.length; i++) {
             pq.offer(enemy[i]);
-            if (n < 0) {
-                if (k > 0) {
-                    n += pq.poll();
-                    k--;
-                } else break;
+            if (pq.size() > k) {
+                n -= pq.poll();
             }
-            answer++;
+            if (n < 0) return i;
         }
-        return answer;
+        return enemy.length;
     }
 }
